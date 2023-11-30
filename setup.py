66d2ad86aee7cb8887087838c1fa5a407de4acdd
Lev Kozlov <kozlov.l.a10@gmail.com>
2023-11-30 08:31:15 +0300

    chore: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="darli",
-    version="0.1.0",
+    version="0.1.1",
     description="The DARLi is a Python 3 library that supports both numerical and symbolical computations of open loop articulated robots provided urdf file.",
     long_description_content_type="text/markdown",
     url="https://github.com/simeon-ned/darli",
