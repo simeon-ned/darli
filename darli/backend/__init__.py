2f0c42b8ac580c3e60fff24d4f06b9160835c85d
Lev Kozlov <kozlov.l.a10@gmail.com>
2024-01-17 12:54:23 +0300

    style: apply formatter

--- darli/backend/__init__.py
+++ darli/backend/__init__.py
@@ -1,3 +1,10 @@
-from .base import BackendBase, Frame, BodyInfo, ConeBase, PinocchioBased, JointType  # noqa: F401
+from .base import (
+    BackendBase,
+    Frame,
+    BodyInfo,
+    ConeBase,
+    PinocchioBased,
+    JointType,
+)  # noqa: F401
 from .casadi import CasadiBackend, CasadiCone  # noqa: F401
 from .pinocchio import PinocchioBackend, PinocchioCone  # noqa: F401
