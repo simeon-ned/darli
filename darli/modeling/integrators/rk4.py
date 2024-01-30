7de1a29f4989dae251baf0933021aa51b3a17071
Lev Kozlov <kozlov.l.a10@gmail.com>
2024-01-30 17:18:06 +0300

    fix: correct rk4 implementation

--- darli/modeling/integrators/rk4.py
+++ darli/modeling/integrators/rk4.py
@@ -12,27 +12,12 @@ class RK4(Integrator):
         dt: cs.SX | float,
     ):
         nq = state_space.model.nq
-        nv = state_space.model.nv
-
-        q, v = x0[:nq], x0[nq:]
 
         # integrate velocity using runge-kutta method
-        def vdot_func(v_n):
-            return state_space.model.forward_dynamics(q, v_n, qfrc_u)
+        def vdot_func(x):
+            return state_space.state_derivative(x[:nq], x[nq:], qfrc_u)
 
-        integrated_v = v + RK4.__step(v, dt, vdot_func)
-
-        # integrate configuration using pinocchio fancy lie-group
-        # TODO: it is not RK4 on manifold
-        integrated_q = state_space.model.backend.integrate_configuration(
-            dt, q, integrated_v
-        )
-
-        container = state_space.model.backend.math.zeros(nq + nv).array
-        container[:nq] = integrated_q
-        container[nq:] = integrated_v
-
-        return container
+        return x0 + RK4.__step(x0, dt, vdot_func)
 
     @staticmethod
     def __step(y0, h, dydt: Callable[[ArrayLike, ArrayLike], ArrayLike]) -> ArrayLike:
