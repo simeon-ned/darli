bfc0991dae6d8d30219152e5d710198eb9550ef3
Simeon <simkaned@gmail.com>
2024-02-01 15:18:26 +0300

    modified rollout example

--- darli/modeling/integrators/mid_point.py
+++ darli/modeling/integrators/mid_point.py
@@ -3,10 +3,9 @@ from .base import Integrator, StateSpaceBase, ArrayLike, cs
 
 
 class MidPoint(Integrator):
-    def __init__(self, 
-                 state_space: StateSpaceBase):
+    def __init__(self, state_space: StateSpaceBase):
         super().__init__(state_space)
-        
+
     def forward(
         self,
         derivative: ArrayLike,
@@ -14,9 +13,9 @@ class MidPoint(Integrator):
         qfrc_u: ArrayLike,
         dt: cs.SX | float,
     ):
-        k1_log = derivative(x0[:self.nq], x0[self.nq:], qfrc_u)
+        k1_log = derivative(x0[: self.nq], x0[self.nq :], qfrc_u)
 
         k2_exp = self.tangent_step(x0, 0.5 * dt * k1_log)
-        k2_log = derivative(k2_exp[:self.nq], k2_exp[self.nq:], qfrc_u)
+        k2_log = derivative(k2_exp[: self.nq], k2_exp[self.nq :], qfrc_u)
 
         return self.tangent_step(x0, dt * k2_log)
