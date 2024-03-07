ed1968295b0aa469a9728acff1eeaad72155614c
Simeon <simkaned@gmail.com>
2024-03-08 01:21:19 +0300

    added templates for state and parameters variables

---
+++ darli/parametric/_parameters.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+class InertialParameters:
+    def __init__(self, np, nb):
+        self.np = np
+        self.nb = nb
+        self.ndx = np * nb
+        self.vector = np.zeros(self.ndx)
+        self.matrix = np.zeros((self.ndx, self.ndx))
+        self.principal_frame = None
+        self.principle_inertia = None
+
+    def __add__(self, other):
+        result = InertialParameters(self.np, self.nb)
+        result.vector = self.vector + other.vector
+        result.matrix = self.matrix + other.matrix
+        return result
+
+    def __sub__(self, other):
+        result = InertialParameters(self.np, self.nb)
+        result.vector = self.vector - other.vector
+        result.matrix = self.matrix - other.matrix
+        return result
+
+    def add(self, other):
+        self.vector += other.vector
+        self.matrix += other.matrix
+
+    def exp(self):
+        # Implement exponential map
+        pass
+
+    def log(self):
+        # Implement logarithmic map
+        pass
+
+    def tangent(self):
+        # Implement tangent space computation
+        pass
+
+    def jacobians(self):
+        # Implement Jacobian computation
+        pass
+
+    def reiman(self):
+        # Implement Riemannian operations
+        pass
+
+    def projection(self):
+        # Implement projection operation
+        pass
+
+    def euclidian(self):
+        # Implement Euclidean operations
+        pass
+
+
+class LumpedParameters(InertialParameters):
+    def __init__(self, np, nb):
+        super().__init__(np, nb)
+        # Add any additional attributes specific to LumpedParameters
+
+
+class ReducedParameters(InertialParameters):
+    def __init__(self, np, nb):
+        super().__init__(np, nb)
+        # Add any additional attributes specific to ReducedParameters
